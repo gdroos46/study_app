1822e05c55ac3d525a519fb83a46eb12b1acafc4
gdroos46 <gdroos4646@icloud.com>
2018-11-30 15:00:23 +0900

    cancancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  load_and_authorize_resource
 
-  def index
-    @users = User.all
-  end
+  def index; end
 
   def show; end
 
-  def new
-    @user = User.new
-  end
+  def new; end
 
   def create
     @user = User.new(user_params)
