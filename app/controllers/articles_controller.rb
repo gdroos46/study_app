ef6d6228adabfd75be311e26bb9c5724ad16da05
gdroos46 <gdroos4646@icloud.com>
2018-12-12 11:06:39 +0900

    article crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[new edit create update destroy]
   load_and_authorize_resource
   load_and_authorize_resource through: :user
 
