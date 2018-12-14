48d428862d64f95aaa480f847b526c28d2a6ac57
gdroos46 <gdroos4646@icloud.com>
2018-12-15 02:03:47 +0900

    comment追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181204040735) do
+ActiveRecord::Schema.define(version: 20181214165209) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20181204040735) do
     t.string "status", default: "draft"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
