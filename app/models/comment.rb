3c595d05ef358274900ef501eb763e79f082a4d7
gdroos46 <gdroos4646@icloud.com>
2018-12-15 02:15:09 +0900

    comment,model作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  to_belongs :user
+  to_belongs :article
+
+  validates :text, presence: true
 end
--- db/migrate/20181214165209_create_comments.rb
+++ db/migrate/20181214165209_create_comments.rb
@@ -3,8 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.belongs_to :user, index: true
       t.belongs_to :article, index: true
-      t.integer :rating
-      t.text :comment
+      t.text :text
 
       t.timestamps
     end
